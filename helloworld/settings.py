dcec11c33708df4c51f734bc51c5e289c30a29ed
Ramrekha yadav <94756876+Ramyadav08@users.noreply.github.com>
2023-10-10 21:35:27 +0530

    Update settings.py

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a-o*iy-twyl3+!pr^@*)v)-@%qr*!r1^shb%$&jnj3qq9oxc2o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0/0']
 
 
 # Application definition
