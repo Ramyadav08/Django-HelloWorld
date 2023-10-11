f9e8a594d2b4ff7bb1cb99f1ccdbe2091c39016c
Ramrekha yadav <94756876+Ramyadav08@users.noreply.github.com>
2023-10-11 09:49:35 +0530

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def sayHello(request):
-    return HttpResponse('<h1 style="color:red">Hello World !! Learn From AapTatt</h1>')
+    return HttpResponse('<h1 style="color:red">Hello World !!? Learn From AapTatt</h1>')
